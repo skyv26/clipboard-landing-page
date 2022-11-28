6d4f1d032b19eee2277b4829d41d1a26630026f1
Aakash Verma <dev.aakashv@gmail.com>
2022-11-28 15:08:01 +0530

    Add the `Image` component as svg for dynamicness

--- src/svg_components/Logo.js
+++ src/svg_components/Logo.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Image } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 
 function Logo({ w, h }) {
   return (
-    <Box w={w} h={h}>
-      <svg viewBox="0 0 125 125" xmlns="http://www.w3.org/2000/svg">
-        <g stroke="#26BBA4" strokeWidth="10" fill="none" fillRule="evenodd">
-          <circle cx="62.5" cy="62.5" r="57.5" />
-          <path
-            d="M85.481 85.481c-12.692 12.692-33.27 12.692-45.962 0s-12.692-33.27 0-45.962 33.27-12.692 45.962 0"
-            strokeLinecap="round"
-          />
-        </g>
-      </svg>
-    </Box>
+    <Image as="svg" w={w} h={h} viewBox="0 0 125 125" xmlns="http://www.w3.org/2000/svg">
+      <g stroke="#26BBA4" strokeWidth="10" fill="none" fillRule="evenodd">
+        <circle cx="62.5" cy="62.5" r="57.5" />
+        <path
+          d="M85.481 85.481c-12.692 12.692-33.27 12.692-45.962 0s-12.692-33.27 0-45.962 33.27-12.692 45.962 0"
+          strokeLinecap="round"
+        />
+      </g>
+    </Image>
   );
 }
 
