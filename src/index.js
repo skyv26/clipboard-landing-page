b3be7a1711431407a6c323b4e2abdff983f797b1
Aakash Verma <dev.aakashv@gmail.com>
2022-11-28 14:28:44 +0530

    Add the `custom` breakpoints to the chakra-UI theme

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const breakpoints = {
+  base: '0px',
+  sm: '320px',
+  md: '768px',
+  lg: '960px',
+  xl: '1200px',
+  '2xl': '1366px',
+  '3xl': '1536px',
+};
+
+const theme = extendTheme({ breakpoints });
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <App />
   </ChakraProvider>,
 );
