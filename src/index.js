26a0b5c967c927d6bea2581b017d25339a8071b9
Aakash Verma <dev.aakashv@gmail.com>
2023-04-30 15:15:23 +0530

    feat: add the `App.js` file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
+import App from "./App";
 import { ReactDOM } from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <></>
+    <App/>
 );
