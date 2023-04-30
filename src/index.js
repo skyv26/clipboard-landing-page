042ba60ac5c077334ee1fd3ea01e1d5c218d0292
Aakash Verma <dev.aakashv@gmail.com>
2023-04-30 17:13:21 +0530

    feat: add the `provider` for chakra

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App/>
+    <ChakraProvider>
+        <App/>
+    </ChakraProvider>
 );
