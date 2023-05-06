251a1f73cfe8f1e1877850a40438f4a296003696
Aakash Verma <dev.aakashv@gmail.com>
2023-05-06 17:27:50 +0530

    fix: resolve the `eslinter` errors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import App from "./App";
-import { ChakraProvider } from "@chakra-ui/react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <ChakraProvider>
-        <App/>
-    </ChakraProvider>
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>,
 );
