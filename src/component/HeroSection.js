c82157ff3969e7fb44024f47febe25041aa628e9
Aakash Verma <dev.aakashv@gmail.com>
2023-05-10 05:56:32 +0530

    asset: create `logo` component

--- src/component/HeroSection.js
+++ src/component/HeroSection.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Box } from '@chakra-ui/react';
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <Box as="section">
+
+    </Box>
   )
 };
 
