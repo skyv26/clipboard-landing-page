bb8c4f41c0617b266f049e32441019a9834f48af
Aakash Verma <dev.aakashv@gmail.com>
2023-05-11 11:26:45 +0530

    feat: add the content of the section

--- src/component/FeatureSection.js
+++ src/component/FeatureSection.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from 'react';
 import { Box } from '@chakra-ui/react';
-import CTA from '../HOC/TextDescription';
+import TextDescription from '../HOC/TextDescription';
+
+const data = {
+  heading: 'Keep track of your snippets',
+  description:
+    'Clipboard instantly stores any item you copy in the cloud,\
+    meaning you can access your snippets immediately on all your\
+    devices. Our Mac and iOS apps will help you organize everything.',
+};
 
 const FeatureSection = () => {
   return (
     <Box as="section">
-        <Box>
-
-        </Box>
+      <TextDescription
+        heading={heading}
+        description={description}
+        semanticTag="h2"
+        buttonRequired={false}
+      />
     </Box>
   );
 };
