c823645c0218e9e53b4bb8a4540761523ee5dd08
Aakash Verma <dev.aakashv@gmail.com>
2023-05-11 14:55:47 +0530

    feat: finished the skeleton for the workflow section

--- src/component/WorkflowSection.js
+++ src/component/WorkflowSection.js
@@ -1,49 +1,59 @@
-import React from 'react'
+import React from 'react';
 import { Box, UnorderedList, List } from '@chakra-ui/react';
 import TextDescription from '../HOC/TextDescription';
 import SneakPreview from '../svg_components/SneakPreview';
 import TextSnippet from '../svg_components/TextSnippet';
+import BlackList from '../svg_components/BlackList';
 
 const data = {
-  heading: 'Keep track of your snippets',
-  description:
-    'Clipboard instantly stores any item you copy in the cloud,\
-    meaning you can access your snippets immediately on all your\
-    devices. Our Mac and iOS apps will help you organize everything.',
+  heading: 'Supercharge your workflow',
+  description: "We've got the tools to boost your productivity.",
 };
-
 
 const WorkflowSection = () => {
   const { heading, description } = data;
-  
+
   const workflowList = [
     {
-      CustomImage: SneakPreview,
-      title: 'Quick Search',
-      description: 'Easily search your snippets by content, category, web address, application, and more.'
+      CustomImage: BlackList,
+      title: 'Create blacklists',
+      description:
+        'Ensure sensitive information never makes its way to your clipboard by excluding certain sources.',
     },
     {
       CustomImage: TextSnippet,
-      title: 'iCloud Sync',
-      description: 'Instantly saves and syncs snippets across all your devices.'
+      title: 'Plain text snippets',
+      description:
+        'Remove unwanted formatting from copied text for a consistent look.',
     },
     {
       CustomImage: SneakPreview,
-      title: 'Complete History',
-      description: 'Retrieve any snippets from the first moment you started using the app.'
-    }
+      title: 'Sneak preview',
+      description:
+        'Quick preview of all snippets on your Clipboard for easy access.',
+    },
   ];
 
   return (
-    <Box as="section" >
-      <TextDescription  heading={heading} description={description} semanticTag='h2' buttonRequired={false} />
+    <Box as="section">
+      <TextDescription
+        heading={heading}
+        description={description}
+        semanticTag="h2"
+        buttonRequired={false}
+      />
       <UnorderedList>
-      {workflowList.map(({ CustomImage, title, description }, index) => (
-        <List key={index}>
-          { <CustomImage /> }
-          <TextDescription heading={title} description={description} semanticTag="h3" buttonRequired={false} />
-        </List>
-      ))}
+        {workflowList.map(({ CustomImage, title, description }, index) => (
+          <List key={index}>
+            {<CustomImage />}
+            <TextDescription
+              heading={title}
+              description={description}
+              semanticTag="h3"
+              buttonRequired={false}
+            />
+          </List>
+        ))}
       </UnorderedList>
     </Box>
   );
