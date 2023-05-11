4284d635782b585d4e7148f19057917e39e8c4aa
Aakash Verma <dev.aakashv@gmail.com>
2023-05-11 14:08:33 +0530

    update: finish the skeleton for the `Feature` section

--- src/component/FeatureSection.js
+++ src/component/FeatureSection.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, Image, UnorderedList, List } from '@chakra-ui/react';
 import TextDescription from '../HOC/TextDescription';
+import FeatureImage from '../svg_components/FeatureImage';
 
 const data = {
   heading: 'Keep track of your snippets',
@@ -10,7 +11,23 @@ const data = {
     devices. Our Mac and iOS apps will help you organize everything.',
 };
 
+const featureList = [
+  {
+    title: 'Quick Search',
+    description: 'Easily search your snippets by content, category, web address, application, and more.'
+  },
+  {
+    title: 'iCloud Sync',
+    description: 'Instantly saves and syncs snippets across all your devices.'
+  },
+  {
+    title: 'Complete History',
+    description: 'Retrieve any snippets from the first moment you started using the app.'
+  }
+];
+
 const FeatureSection = () => {
+  const { heading, description } = data;
   return (
     <Box as="section">
       <TextDescription
@@ -19,6 +36,14 @@ const FeatureSection = () => {
         semanticTag="h2"
         buttonRequired={false}
       />
+      <Box>
+        <FeatureImage />
+      </Box>
+      <UnorderedList>
+        {featureList.map((item, index) => (
+            <TextDescription key={index} heading={item.title} description={item.description} semanticTag="h3" buttonRequired={false} />
+          ))}
+      </UnorderedList>
     </Box>
   );
 };
