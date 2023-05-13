28c6d2e091f5fbea69e13f7ebb4151b647829050
Aakash Verma <dev.aakashv@gmail.com>
2023-05-13 19:36:57 +0530

    feat: update the `CTA` section

--- src/component/CTASection.js
+++ src/component/CTASection.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Box } from '@chakra-ui/react';
+import TextDescription from '../HOC/TextDescription';
+
+const data = {
+  heading: "Clipboard for iOS and Mac OS",
+  description: "Available for free on the App Store. Download for Mac or iOS, sync with iCloud and you’re ready to start adding to your clipboard."
+};
 
 const CTASection = () => {
   return (
-    <div>CTASection</div>
+    <Box as="header">
+      <TextDescription heading={heading} description={description} />
+    </Box>
   );
 };
 
