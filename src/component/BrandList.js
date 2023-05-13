f2f31da80467020b7dcf8b7b708eadd7d9afcc8e
Aakash Verma <dev.aakashv@gmail.com>
2023-05-13 19:32:28 +0530

    feat: update the `brand` component with svg component

--- src/component/BrandList.js
+++ src/component/BrandList.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { UnorderedList, List } from '@chakra-ui/react';
+import GoogleIcon from '../svg_components/GoogleIcon';
+import HewlettIcon from '../svg_components/HewlettIcon';
+import IBMIcon from '../svg_components/IBMIcon';
+import MicrosoftIcon from '../svg_components/MicrosoftIcon';
+import VectorGraphics from '../svg_components/VectorGraphics';
 
 const BrandList = () => {
   return (
-    <div>BrandList</div>
+    <UnorderedList>
+      <List>
+        <GoogleIcon />
+        <IBMIcon />
+        <MicrosoftIcon />
+        <HewlettIcon />
+        <VectorGraphics />
+      </List>
+    </UnorderedList>
   );
 };
 
