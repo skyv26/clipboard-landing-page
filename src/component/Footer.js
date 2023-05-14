e7d7ef1932fb77324944054beb19c9e03ed564c3
Aakash Verma <dev.aakashv@gmail.com>
2023-05-14 20:00:45 +0530

    feat: finish the footer skeleton

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,8 +1,43 @@
 import React from 'react'
+import { Box, Grid, GridItem, Link, HStack } from '@chakra-ui/react';
+import Logo from '../svg_components/Logo';
+import FbIcon from '../svg_components/FbIcon';
+import InstaIcon from '../svg_components/InstaIcon';
+import TwitterIcon from '../svg_components/TwitterIcon';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box as="footer">
+      <Logo />
+      <Grid>
+        <GridItem>
+          <Link>FAQs</Link>
+        </GridItem>
+        <GridItem>
+          <Link>Contact Us</Link>
+        </GridItem>
+        <GridItem>
+          <Link>Privacy Policy</Link>
+        </GridItem>
+        <GridItem>
+          <Link>Press Kit</Link>
+        </GridItem>
+        <GridItem>
+          <Link>Install Guide</Link>
+        </GridItem>
+      </Grid>
+    <HStack>
+      <Link>
+        <FbIcon />
+      </Link>
+      <Link>
+        <InstaIcon />
+      </Link>
+      <Link>
+        <TwitterIcon />
+      </Link>
+    </HStack>
+    </Box>
   );
 };
 
