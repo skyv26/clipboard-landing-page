a5367c86463f8e16e71f2265fee0e5feeb544957
Aakash Verma <dev.aakashv@gmail.com>
2023-05-20 19:50:53 +0530

    feat: add the `chakra-ui` flexbox and styles for the section

--- src/component/AccessSection.js
+++ src/component/AccessSection.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 import TextDescription from '../HOC/TextDescription';
 
 const data = {
@@ -12,9 +12,18 @@ const data = {
 function AccessSection() {
   const { heading, description } = data;
   return (
-    <Box as="section" padding={['16px']}>
-      <TextDescription heading={heading} description={description} buttonRequired={false} semanticTag="h2" />
-    </Box>
+    <Flex as="section" textAlign={['center']} padding={['16px']} flexDirection={['column']} pt={[24]} gap={['13px']}>
+      <TextDescription
+        heading={heading}
+        description={description}
+        headingSize={['20px']}
+        descriptionSize={['13px']}
+        headingWeight={600}
+        descriptionWeight={400}
+        buttonRequired={false}
+        semanticTag="h2"
+      />
+    </Flex>
   );
 }
 
