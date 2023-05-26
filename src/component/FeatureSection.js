d8529cc7a0cb35fdfcdba409a039993aca991dd5
Aakash Verma <dev.aakashv@gmail.com>
2023-05-26 18:51:21 +0530

    feat: use the `Flex` component and `ES6` standards

--- src/component/FeatureSection.js
+++ src/component/FeatureSection.js
@@ -29,27 +29,29 @@ const featureList = [
 const FeatureSection = () => {
   const { heading, description } = data;
   return (
-    <Flex as="section" textAlign={['center']} padding={['16px']} flexDirection={['column']} pt={[24]} gap={['13px']}>
-      <TextDescription
-        heading={heading}
-        description={description}
-        headingSize={[28]}
-        headingWeight={[600]}
-        descriptionSize={[13]}
-        semanticTag="h2"
-        buttonRequired={false}
-      />
-      <Flex flexDirection={['column']} alignItems={['center']} gap={['35px']}>
+    <Flex as="section" textAlign={['center']} padding={{ base: '16px', xs: '18px', sm: '36px' }} flexDirection={['column']} paddingTop={{ base: 24, xs: 36, sm: 40 }} gap={{ base: 12, xs: 16 }}>
+      <Flex flexDirection={{ base: 'column' }} gap={{ base: 13, xs: '16px' }}>
+        <TextDescription
+          heading={heading}
+          description={description}
+          headingSize={{ base: 28 }}
+          headingWeight={[600]}
+          descriptionSize={{ base: 13, xs: 16 }}
+          semanticTag="h2"
+          buttonRequired={false}
+        />
+      </Flex>
+      <Flex flexDirection={['column']} alignItems={['center']} gap={{ base: '35px', xs: '55px' }}>
         <FeatureImage mt={['35px']} w={['full']} h={['auto']} />
-        <UnorderedList display={['flex']} flexDirection={['column']} alignItems={['center']} w={['full']} textAlign={['center']} m={['auto']} gap={[6]}>
+        <UnorderedList display={['flex']} flexDirection={['column']} alignItems={['center']} w={['full']} textAlign={['center']} m={['auto']} gap={{ base: 6, xs: 10, sm: 12 }}>
           {featureList.map((item) => (
             <ListItem key={item.title} display={['flex']} flexDirection={['column']} gap={['10px']}>
               <TextDescription
                 heading={item.title}
                 description={item.description}
                 semanticTag="h3"
-                headingSize={['20px']}
-                descriptionSize={['13px']}
+                headingSize={{ base: '20px', xs: '24px', sm: '26px' }}
+                descriptionSize={{ base: '13px', xs: '16px', sm: '18px' }}
                 headingWeight={600}
                 descriptionWeight={400}
                 buttonRequired={false}
