1a0b0574691232097ddff8406aa13c39656d2293
Aakash Verma <dev.aakashv@gmail.com>
2023-10-01 06:12:45 +0530

    feat: add the skeleton for the hero section using re-useable component

--- src/component/HeroSection.js
+++ src/component/HeroSection.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import { Box } from '@chakra-ui/react';
+import { Box, Text, Link as CallToActionButton } from '@chakra-ui/react';
+import CTA from '../HOC/CTA';
+import Logo from '../svg_components/Logo';
+
+const data = {
+  heading: "A history of everything you copy",
+  description: "Clipboard allows you to track and organize everything you copy. Instantly access your clipboard on all your devices."
+};
 
 const HeroSection = () => {
+  const { heading, description } = data;
   return (
     <Box as="section">
-
+      <Logo />
+      <CTA heading description />
     </Box>
   )
 };
